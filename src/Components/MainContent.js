735ed9751143f7037899a3e48227d0de2cd806c9
juda-star <juda.ishta@gmail.com>
2021-08-08 00:15:07 +0300

    juda search

---
+++ src/Components/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+//  import '../assets/scss/main.scss'
+function MainContent() {
+  return (
+    <main>
+      <div className="main-head">
+        <form className="search-box">
+          <input type="search" placeholder="search for an anime..." required />
+        </form>
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
