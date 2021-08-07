52e44f71c6a4ac015dff7b9251fe02088ab43fc6
yosefsahalo <yosefsahalu@gmail.com>
2021-08-08 00:26:11 +0300

    sidebar final changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-	import { useState,useEffect } from "react";
-	import Header from "./Components/Header";
-	import Sidebar from "./Components/Sidebar";
-	function App() {
-const [animeList, setAnimeList] = useState([]);
-const [topAnime, setTopAnime] = useState([]);
-const [searct, setSearct] = useState('');
-		return (
-			<div className="App">
-				<Header/>
-				<div className="content-wrap">
-				<Sidebar topAnime={topAnime}/>
-				</div>
+import { useState, useEffect } from "react";
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
 
-			</div>
-		);
-	}
+function App() {
+  const [animeList, setAnimeList] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+  const [searct, setSearct] = useState("");
+
+  const GetTopAnime = async () => {
+	  const temp = await fetch('https://api.jikan.moe/v3/top/anime/1/bypopularity')
+	  .then(res => res.json());
+
+	  setTopAnime(temp.top.slice(0,5));
+  }
+
+  useEffect(() => {
+	GetTopAnime();
+
+	console.log('Top Anime');
+  }, [])
+
+  console.log(topAnime);
 
 
-	export default App;
+  return (
+    <div className="App">
+      <Header />
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+      </div>
+    </div>
+  );
+}
 
-
+export default App;
