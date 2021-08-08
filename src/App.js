686f06a3fb14fbdbfbd0d0e4836f3a07e323b46b
juda-star <juda.ishta@gmail.com>
2021-08-08 17:19:45 +0300

    app

--- src/App.js
+++ src/App.js
@@ -2,56 +2,23 @@ import { useState, useEffect } from "react";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
 
-
-function App() {
-  const [animeList, setAnimeList] = useState([]);
-  const [topAnime, setTopAnime] = useState([]);
-  const [searct, setSearct] = useState("");
-
-  const GetTopAnime = async () => {
-	  const temp = await fetch('https://api.jikan.moe/v3/top/anime/1/bypopularity')
-	  .then(res => res.json());
-
-	  setTopAnime(temp.top.slice(0,5));
-  }
-
-  useEffect(() => {
-	GetTopAnime();
-
-	console.log('Top Anime');
-  }, [])
-
-  console.log(topAnime);
-
-
-  return (
-    <div className="App">
-      <Header />
-      <div className="content-wrap">
-        <Sidebar topAnime={topAnime} />
-      </div>
-    </div>
-  );
-}
-
-
 import MainContent from "./Components/MainContent";
 function App() {
   const [animeList, setAnimeList] = useState([]);
   const [topAnime, setTopAnime] = useState([]);
   const [searct, setSearct] = useState("");
-  const GetTopAnime=async()=>{
-	  const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
-	  .then(res=>res.json());
+  const GetTopAnime = async () => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/top/anime/1/bypopularity`
+    ).then((res) => res.json());
 
-	  setTopAnime(temp.top.slice(0,5));
-  }
+    setTopAnime(temp.top.slice(0, 5));
+  };
 
   useEffect(() => {
-	GetTopAnime();
-  }, [])
+    GetTopAnime();
+  }, []);
   console.log(topAnime);
-
 
   return (
     <div className="App">
@@ -63,6 +30,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
